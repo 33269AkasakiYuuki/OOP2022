44f2d149abd3bab05084c277fbcfa4c590e19a42
雄生 赤崎 <33269@ojs.ac.jp>
2022-05-10 13:33:05 +0900

    第2章演習問題新規作成

---
+++ Chapter02/SalesCalculator/SelesCounter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator {
+    public class SelesCounter {
+        private List<Sale> _sales; //csvファイルから読み込んだデータ
+
+        //コンストラクタ
+        public SelesCounter(List<Sale> sales) {
+            _sales = sales;
+        }
+
+        //店舗別売り上げを求める
+        public IDictionary<string, int> GetValuePairs() {
+            var dict = new Dictionary<string, int>();
+            foreach (Sale sale in _sales) {
+                if (dict.ContainsKey(sale.ShopName))
+                    dict[sale.ShopName] += sale.Amount;
+                    else
+                        dict[sale.ShopName] = sale.Amount;
+                }
+            }
+            return dict;
+        }
+
+}
+
