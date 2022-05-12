8d8ec0b96e0f5146cd85796650913bd71a95a11b
雄生 赤崎 <33269@ojs.ac.jp>
2022-05-12 14:12:11 +0900

    問題2.2完成

---
+++ Chapter02/Exercise02/FeetConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    //フィートとメートルの単位変換クラス
+    public static class FeetConverter {
+        private const double retio = 0.0254; //定数retioを定義
+        //public static readonly double retio = 0.3048; //←外部からもアクセスさせたい場合 
+
+        //メートルからフィートを求める(静的メソッド)
+        public static double fromMeter(double meter) {
+            return meter / retio;
+        }
+
+        //フィートからメートルを求める(静的メソッド)
+        public static double ToMeter(double feet) {
+            return feet * retio;
+        }
+    }
+}
--- Chapter02/Exercise02/Program.cs
+++ Chapter02/Exercise02/Program.cs
@@ -7,6 +7,31 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
+            if (args.Length >= 1 && args[0] == "-tom") {
+                //インチからメートルへの対応表を出力
+                PrintFeetToMeterList(1, 10);
+            }
+            else {
+                //メートルからインチへの対応表を出力
+                PrintMeterToFeetList(1, 10);
+            }
+        }
+
+        //インチからメートルへの対応表を出力
+        private static void PrintFeetToMeterList(int start, int stop) {
+            for (int feet = 1; feet <= 10; feet++) {
+                double meter = FeetConverter.ToMeter(feet);
+                Console.WriteLine("{0} cm = {1:0.0000} m", feet, meter);
+            }
+        }
+
+        //メートルからインチへの対応表を出力
+        private static void PrintMeterToFeetList(int start, int stop) {
+            for (int meter = 1; meter <= 10; meter++) {
+                double feet = FeetConverter.fromMeter(meter);
+                Console.WriteLine("{0} m = {1:0.0000} cm", meter, feet);
+            }
         }
     }
 }
+
