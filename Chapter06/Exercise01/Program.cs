562d6d79c2b6d397098b3de5fe625317b03557cc
雄生 赤崎 <33269@ojs.ac.jp>
2022-05-31 15:57:30 +0900

    問題6.1.2完成

---
+++ Chapter06/Exercise01/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+            var numbers = new int[] { 5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35 };
+
+            Exercise1_1(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_2(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_3(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_4(numbers);
+            Console.WriteLine("-----");
+
+            Exercise1_5(numbers);
+        }
+        
+        //最大値
+        private static void Exercise1_1(int[] numbers) {
+            var max = numbers.Max();
+            Console.WriteLine(max);
+        }
+
+        //最後から２つの要素取り出し
+        private static void Exercise1_2(int[] numbers) {
+            /* for (int i = numbers.Length-2; i < numbers.Length; i++) {
+                 Console.WriteLine(numbers[i]);
+             }*/
+            foreach (var n in numbers.Skip(numbers.Length -2)) {
+                Console.WriteLine(n);
+            }
+        }
+
+        //数値を文字列へ変換
+        private static void Exercise1_3(int[] numbers) {
+        }
+
+        private static void Exercise1_4(int[] numbers) {
+        }
+
+        private static void Exercise1_5(int[] numbers) {
+        }
+    }
+}
