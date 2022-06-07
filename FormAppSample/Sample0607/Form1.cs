fa9bcfdeb7ba027037a8a1851334488588a2e3e5
雄生 赤崎 <33269@ojs.ac.jp>
2022-06-07 14:40:54 +0900

    Sample0607

---
+++ FormAppSample/Sample0607/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sample0607 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+             if (int.Parse(tbNum2.Text) != 0) {
+                 tbAns.Text = (int.Parse(tbNum1.Text) / int.Parse(tbNum2.Text)).ToString();
+                 tbMod.Text = (int.Parse(tbNum1.Text) % int.Parse(tbNum2.Text)).ToString();
+             }
+             else {
+                 MessageBox.Show("0で割り算できません", "エラー",
+                     MessageBoxButtons.OK,
+                     MessageBoxIcon.Error);
+             }
+
+            if (nudNum2.Value !=0) {
+                nudAns.Value = nudNum1.Value / nudNum2.Value;
+                nudMod.Value = nudNum1.Value % nudNum2.Value;
+            }
+            else {
+                MessageBox.Show("0で割り算できません", "エラー",
+                     MessageBoxButtons.OK,
+                     MessageBoxIcon.Error);
+            }
+
+
+        }
+    }
+}
