fdeb79072e9d069ae4180111a5426732c23c520b
雄生 赤崎 <33269@ojs.ac.jp>
2022-07-01 13:27:31 +0900

    rss　途中

---
+++ Chapter011/XmlSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace XmlSample {
+    class Program {
+        static void Main(string[] args) {
+
+            var wc = new WebClient();
+            var stream = new WebClient().OpenRead("https://news.yahoo.co.jp/rss/media/kurumans/all.xml");
+
+            var xdoc = XDocument.Load(stream);
+            var xNews = xdoc.Root.Descendants("item");
+
+            foreach (var data in xNews) {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
