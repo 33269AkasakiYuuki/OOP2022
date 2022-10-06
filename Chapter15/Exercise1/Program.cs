2fe92739bbb2c9e0af31d946144d13214b8dc8de
雄生 赤崎 <33269@ojs.ac.jp>
2022-10-06 15:18:55 +0900

    15章　演習問題　Exercise1_2 完成

--- Chapter15/Exercise1/Program.cs
+++ Chapter15/Exercise1/Program.cs
@@ -25,13 +25,26 @@ namespace Exercise1 {
         }
 
         private static void Exercise1_2() {
+            var max = Library.Books.Max(b => b.Price);
+            var selected = Library.Books
+                .Where(b => b.Price == max);
+            
+            foreach(var book in selected) {
+                Console.WriteLine($"発行年:{book.PublishedYear} カテゴリ:{book.CategoryId} 価格:{book.Price} タイトル:{book.Title} ");
+            }
         }
 
         private static void Exercise1_3() {
+
         }
 
         private static void Exercise1_4() {
-
+            var selected = Library.Books
+                .OrderByDescending(b => b.PublishedYear)
+                .ThenByDescending(b => b.Price);
+            foreach(var book in selected) {
+                Console.WriteLine($"{book.PublishedYear} {book.Price} {book.Title} ");
+            }
         }
 
         private static void Exercise1_5() {
